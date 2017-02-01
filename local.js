f5104b032cb22cc56e91dd424549b5613af5f497
edge <functaire@gmail.com>
2017-01-31 16:31:25 -0800

    fix index

---
+++ local.js
@@ -0,0 +1,35 @@
+const { prompt } = require('./util')
+const { execSync, spawnSync } = require('child_process')
+const rl = require('readline-sync')
+
+rl.setDefaultOptions({ prompt: '' })
+
+const merge = (...args) => Object.assign({}, ...args)
+
+const register = (name, ...ts) => {
+	__synthesizer__tasks__.set(name, ts)
+}
+
+const run = (file, args = [], options = {}) => {
+	const execution = spawnSync(file, args, merge({ stdio: 'inherit' }, options))
+	if (execution.status !== 0) {
+		throw new Error(`${ file }[${ args.join(', ') }] exited with status ${ execution.status }`)
+	}
+	return execution
+}
+
+const shell = (cmd, options = {}) => execSync(cmd, merge({ stdio: 'inherit', options }))
+
+const ask = (question, options = {}) => {
+	if (question.length > 0) prompt(question)
+	return rl.prompt(options)
+}
+
+module.exports = {
+	register,
+	perform: __synthesizer__perform__,
+	run,
+	shell,
+	ask
+}
+
